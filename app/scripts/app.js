399096e043986ee1805a9a23142029307f5e7659
jshemas <jshemas@hotmail.com>
2015-03-30 16:43:56 -0400

    getting ride of routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,6 @@
-angular.module('jsApp', ['ngRoute']).config(
+angular.module('jsApp', []).config(
 	[
 		function (
 		) {}
 	]
 )
-.run(['$route', function($route) {
-	$route.reload();
-}])
-.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider
-		.when('/home', {
-			templateUrl: '/views/home'
-		})
-		.otherwise({
-			redirectTo: '/home'
-		});
-}]);
